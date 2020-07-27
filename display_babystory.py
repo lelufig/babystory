b48011e5b22f7fdf7eac6f3bf77e0abb7e39e926
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-07-27 10:49:11 +0900

    :art: 日付のフィルター機能を追加

--- display_babystory.py
+++ display_babystory.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from getpass import getpass
 import time
+from datetime import datetime
 
 
 def get_target_urls(file_name):
@@ -25,7 +26,7 @@ def login(chrome, username, password):
     time.sleep(1)
 
 
-def get_posts_by_keyword(chrome, target_urls):
+def get_posts_by_keyword(chrome, target_urls, filter_date):
     print('=' * 80)
     for target_url in target_urls:
         try:
@@ -35,29 +36,39 @@ def get_posts_by_keyword(chrome, target_urls):
             # ターゲットとなるユーザの投稿件数を取得
             poster_name = chrome.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/header/div[2]/div[1]/div[1]/span/a').text
             post_body = chrome.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/div[1]/ul/div/li/div/div/div[2]/span').text
+            post_date_str = chrome.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/div[2]/a/time').get_attribute('datetime')
+            post_date = datetime(year=int(post_date_str[0:4]), month=int(post_date_str[5:7]), day=int(post_date_str[8:10]))
 
             # ここから表示
-            print('🌟', poster_name, 'さんの投稿')
-            print(target_url)
-            print(post_body)
+            if post_date >= filter_date:
+                print('🌟', poster_name, 'さんの投稿', post_date)
+                print(target_url)
+                print(post_body)
+                print('=' * 80)
+
+        except KeyboardInterrupt:
+            print()
+            if 'n' ==input('Continue?[y/n]: '):
+                break
 
         except:
             print('エラーが発生しました')
             print(target_url)
-        print('=' * 80)
-        print()
+            print('=' * 80)
 
 
 def main():
     # 初期設定
     username = input('Username: ')
     password = getpass('Password: ')
+    filter_date = datetime.strptime(input('Date after: '), '%Y-%m-%d')
+    print('Collect posts after - ', filter_date)
     target_urls = get_target_urls('post_urls.txt')
     chrome = webdriver.Chrome()
 
     # 処理
     login(chrome, username, password)
-    get_posts_by_keyword(chrome, target_urls)
+    get_posts_by_keyword(chrome, target_urls, filter_date)
 
 
 if __name__=='__main__':
